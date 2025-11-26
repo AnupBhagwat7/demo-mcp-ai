07541dde9d9c4c85a2095bfcd05a6b97278312e2
Anup Bhagwat <anup.bhagwat7@gmail.com>
2025-11-26 06:14:46 +0530

    Spring boot initializer project

---
+++ src/main/java/com/example/demomcpai/DemoMcpAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demomcpai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMcpAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMcpAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demomcpai/DemoMcpAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demomcpai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMcpAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
